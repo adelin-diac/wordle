0367ce2f58c8e8ce4e289c2279d1135c7666b668
adelin-diac <adelin334@gmail.com>
2024-08-28 09:55:40 +0100

    init spring boot files

---
+++ v2_with_server/wordle/src/test/java/site/adelindiac/wordle/WordleApplicationTests.java
@@ -0,0 +1,13 @@
+package site.adelindiac.wordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
